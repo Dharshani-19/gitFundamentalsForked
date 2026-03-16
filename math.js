ff8437b454cbe077640f32d127a9a019af696e54
SapnaSakthivel1003 <sapna.s@mitrahsoft.in>
2026-03-16 13:06:26 +0530

    first commit

---
+++ math.js
@@ -0,0 +1,15 @@
+const add=(a,b)=>{
+    return a+b;
+}
+console.log(add(10,5));
+
+const sub=(a,b)=>{
+    return a-b;
+}
+console.log(sub(10,5));
+
+
+const mul=(a,b)=>{
+    return a*b;
+}
+console.log(mul(10,5));
